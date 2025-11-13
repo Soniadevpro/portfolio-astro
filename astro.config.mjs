bb171fbb9f1df5a0b1abd09196a733b0c73f28b6
Sonia <143387635+Soniadevpro@users.noreply.github.com>
2025-11-13 12:31:08 +0000

    Mise en place de la structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
-import {defineConfig} from 'astro/config' ;
+import { defineConfig } from 'astro/config';
 
-export default defineConfig ({
+export default defineConfig({
 
-
-
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@layouts': '/src/layouts',
+        '@pages': '/src/pages',
+        '@styles': '/src/styles',
+      },
+    },
+  },
 });
